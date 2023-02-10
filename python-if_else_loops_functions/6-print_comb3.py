ad3eb73ae2dd9d2f372ee60b9b90dfac86a33bcf
mulimuoki001 <b.muoki@alustudent.com>
2023-02-10 00:06:18 -0800

    smart

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -6,4 +6,4 @@ for digit1 in range(0, 10):
         if digit1 == 8 and digit2 == 9:
             print("{}{}".format(digit1, digit2))
         else:
-            print("{}{}".format(digit1, digit
+            print("{}{}".format(digit1, digit2), end=", ")
