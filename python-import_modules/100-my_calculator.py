7f07aaaa33f1495cf6500d25635e6c59cd7e45d3
mulimuoki001 <b.muoki@alustudent.com>
2023-02-28 13:41:26 +0200

    dont fumble

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,1 +1,23 @@
-:w
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    if len(sys.argv) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <bi>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if op == "+":
+        result = add(a, b)
+    elif op == "-":
+        result = sub(a, b)
+    elif op == "*":
+        result = mul(a, b)
+    elif op == "/":
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
