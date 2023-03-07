02be157524943d20e72285073d9be9e7d46d50b1
mulimuoki001 <b.muoki@alustudent.com>
2023-03-07 02:25:31 -0800

    ahh

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 
-new_matrix = []
 def square_matrix_simple(matrix=[]):
+    new_matrix = []
     for row in matrix:
-         new_row = list(map(lambda a: a**2, row))
-         new_matrix.append(new_row)
+        new_row = list(map(lambda a: a**2, row))
+        new_matrix.append(new_row)
     return new_matrix
