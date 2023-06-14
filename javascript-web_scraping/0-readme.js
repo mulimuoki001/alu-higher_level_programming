578c2ac48b33baa3f4a163af7982ad4628cf39ad
mulimuoki001 <b.muoki@alustudent.com>
2023-06-14 03:22:54 +0200

    javascript web scraping

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const file = process.argv.slice(2)[0];
+const fs = require('fs');
+fs.readFile(file, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
